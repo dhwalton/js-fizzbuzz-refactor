cd5b2c74f2b552987de0a01a40e7a516b36331d0
dhwalton <donny77walton@gmail.com>
2016-05-22 13:14:00 -0400

    FizzBuzz with validated user input

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	function fizzBuzz(max) {
+		var result = "";
+		for (var i = 1; i <= max; i++) {
+			var thisNum = "";
+			if (i % 3 == 0) {
+				thisNum += "Fizz";
+			}
+			if (i % 5 == 0) {
+				thisNum += "Buzz";
+			}
+			if (thisNum == "") {
+				thisNum = i;
+			}
+			result += thisNum + "<br />";
+		}
+		return result;
+	}
+
+	var fizzBuzzMax = "+" + prompt("Enter the max number for fizzin' and buzzin'");
+
+	while (isNaN(fizzBuzzMax) || fizzBuzzMax < 1 || fizzBuzzMax % 1 != 0) {
+		fizzBuzzMax = parseInt(prompt("The max number must be an integer greater than 0!  Try again."));
+	}
+
+	$('body').append(fizzBuzz(fizzBuzzMax));
+
+});
